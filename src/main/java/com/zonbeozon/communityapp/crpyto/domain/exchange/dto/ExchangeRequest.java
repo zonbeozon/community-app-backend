e22ca1f2f73056547c5a630a7d307e7b90b193a1
jintakkim <mine407603@gmail.com>
2025-01-19 16:27:20 +0900

    feat(ExchangeRequest): 거래소 생성 요청 Dto

---
+++ src/main/java/com/zonbeozon/communityapp/crpyto/domain/exchange/dto/ExchangeRequest.java
@@ -0,0 +1,12 @@
+package com.zonbeozon.communityapp.crpyto.domain.exchange.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ExchangeRequest {
+    private String englishName;
+    private String koreanName;
+    private String description;
+}
