6e59eba25f0d023355172eb9f278a262825efde7
jintakkim <mine407603@gmail.com>
2025-01-19 16:12:37 +0900

    feat(MarketFetcher): MarketFetcher 인터페이스 구현

---
+++ src/main/java/com/zonbeozon/communityapp/crpyto/fetch/MarketFetcher.java
@@ -0,0 +1,7 @@
+package com.zonbeozon.communityapp.crpyto.fetch;
+
+import com.zonbeozon.communityapp.crpyto.fetch.dto.MarketFetchResult;
+
+public interface MarketFetcher {
+    MarketFetchResult fetch();
+}
