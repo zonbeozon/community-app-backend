1941264de4a73909461b22bb8e0b3c0a172a931e
jintakkim <mine407603@gmail.com>
2025-01-19 16:01:51 +0900

    feat(MarketRepository): MarketRepository 구현

---
+++ src/main/java/com/zonbeozon/communityapp/crpyto/domain/market/repository/MarketRepository.java
@@ -0,0 +1,7 @@
+package com.zonbeozon.communityapp.crpyto.domain.market.repository;
+
+import com.zonbeozon.communityapp.crpyto.domain.market.Market;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MarketRepository extends JpaRepository<Market, Long> {
+}
