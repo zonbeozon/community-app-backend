172b7c613e0db458e96e3ecc7974a3b81bd9764e
jintakkim <mine407603@gmail.com>
2025-01-19 16:37:21 +0900

    test(UpbitMarketFetcher): UpbitMarketFetcher 테스트

---
+++ src/test/java/com/zonbeozon/communityapp/crypto/fetch/upbit/UpbitMarketFetcherTest.java
@@ -0,0 +1,7 @@
+package com.zonbeozon.communityapp.crypto.fetch.upbit;
+
+import com.zonbeozon.communityapp.crpyto.fetch.upbit.UpbitMarketFetcher;
+
+public class UpbitMarketFetcherTest {
+    private UpbitMarketFetcher upbitMarketFetcher;
+}
