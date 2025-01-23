c7d0b76fd48b0a28193009784425fbc10c8f031c
jintakkim <mine407603@gmail.com>
2025-01-23 12:19:11 +0900

    feat: 에러 코드 및 커스텀 예외 구현
    runtimeException 대신 errorCode 필드를 가지고 있는 CustomException을 구현한다.

---
+++ src/main/java/com/zonbeozon/communityapp/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.zonbeozon.communityapp.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    // auth
+    ILLEGAL_REGISTRATION_ID(NOT_ACCEPTABLE, "illegal registration id"),
+    TOKEN_EXPIRED(UNAUTHORIZED, "토큰이 만료되었습니다."),
+    INVALID_TOKEN(UNAUTHORIZED, "올바르지 않은 토큰입니다."),
+    INVALID_JWT_SIGNATURE(UNAUTHORIZED, "잘못된 JWT 시그니처입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
