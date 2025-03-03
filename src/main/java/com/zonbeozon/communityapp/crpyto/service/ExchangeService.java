2ead38cfcf4682062e4e25eaefaba61778f74970
jintakkim <mine407603@gmail.com>
2025-03-03 11:09:14 +0900

    chore: 기본 설정 거래소 추가

--- src/main/java/com/zonbeozon/communityapp/crpyto/service/ExchangeService.java
+++ src/main/java/com/zonbeozon/communityapp/crpyto/service/ExchangeService.java
@@ -64,24 +64,27 @@ public class ExchangeService {
 
     @PostConstruct
     @Transactional
-    public void setup() {
+    public void addDefaultExchanges() {
         addExchange(Exchange.builder()
                 .englishName("upbit")
                 .koreanName("업비트")
-                .description("생략")
+                .englishDescription("Upbit is the largest cryptocurrency exchange in South Korea in terms of trading volume and customer base, with over 180 crypto coins/tokens listed and over 300 trading pairs readily available. Upbit's ecosystem comprises of Upbit Exchange, NFT Marketplace/Drops platform, and its Staking services.")
                 .topPriorityMarketType(MarketType.KRW)
+                .logo("https://s2.coinmarketcap.com/static/img/exchanges/64x64/351.png")
                 .build());
         addExchange(Exchange.builder()
                 .englishName("bithumb")
                 .koreanName("빗썸")
-                .description("생략")
+                .englishDescription("Bithumb is one of South Korea's largest crypto exchanges, with $205 million in daily trading volume at the time of writing and with 170+ listed cryptocurrencies.")
                 .topPriorityMarketType(MarketType.KRW)
+                .logo("https://s2.coinmarketcap.com/static/img/exchanges/64x64/200.png")
                 .build());
         addExchange(Exchange.builder()
                 .englishName("binance")
                 .koreanName("바이낸스")
-                .description("생략")
+                .englishDescription("Binance is the world’s largest crypto exchange by trading volume, with $76 billion daily trading volume on Binance exchange as of August 2022, and 90 million customers worldwide. The platform has established itself as a trusted member of the crypto space, where users can buy, sell and store their digital assets, as well as access over 350 cryptocurrencies listed and thousands of trading pairs. The Binance ecosystem now comprises of Binance Exchange, Labs, Launchpad, Info, Academy, Research, Trust Wallet, Charity, NFT and more.")
                 .topPriorityMarketType(MarketType.USDT)
+                .logo("https://s2.coinmarketcap.com/static/img/exchanges/64x64/270.png")
                 .build());
     }
 }
