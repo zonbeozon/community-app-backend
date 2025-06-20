9409fc33552316cacbcbbb1c47df65c5a4a0d792
jintakkim <mine407603@gmail.com>
2025-06-20 17:50:48 +0900

    ChannelAddBadRequestException 헨들러 기능 추가

--- src/main/java/com/zonbeozon/channel/controller/ChannelExceptionHandler.java
+++ src/main/java/com/zonbeozon/channel/controller/ChannelExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.zonbeozon.channel.controller;
 
 import com.zonbeozon.channel.exception.ChannelAddBadRequestException;
-import com.zonbeozon.channel.exception.ChannelBadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.zonbeozon.channel")
 public class ChannelExceptionHandler {
-    @ExceptionHandler(ChannelBadRequestException.class)
+    @ExceptionHandler(ChannelAddBadRequestException.class)
     public ResponseEntity<?> handleChannelBadRequestException(ChannelAddBadRequestException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.toResponse());
     }
---
+++ src/test/java/com/zonbeozon/post/PostServiceTest.java
@@ -0,0 +1,16 @@
+package com.zonbeozon.post;
+
+import com.zonbeozon.post.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTest {
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @DisplayName("")
+}
