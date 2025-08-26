ed2aab30c0df4bf2d0024a06ba9ae6c800e74d1d
jintakkim <mine407603@gmail.com>
2025-08-26 14:42:34 +0900

    test: comment 생성 조회 테스트

--- src/test/java/com/zonbeozon/comment/service/CommentAssemblerTest.java
+++ src/test/java/com/zonbeozon/comment/service/CommentAssemblerTest.java
@@ -1,11 +1,45 @@
 package com.zonbeozon.comment.service;
 
 import com.zonbeozon.base.AbstractChannelIntegrationTest;
+import com.zonbeozon.channel.entity.BlogChannel;
+import com.zonbeozon.comment.dto.CommentsWithAuthorResponse;
+import com.zonbeozon.comment.entity.Comment;
+import com.zonbeozon.member.domain.Member;
+import com.zonbeozon.post.entity.Post;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CommentAssemblerTest extends AbstractChannelIntegrationTest {
+    @Autowired
     private CommentAssembler commentAssembler;
 
-//    @DisplayName("postId에 연관되어 있는 comment가 호출된다.")
-//    public
+    private Member member;
+    private BlogChannel blogChannel;
+    private Post post;
+
+    @BeforeEach
+    void setup() {
+        member = testMemberService.createAndSave();
+        blogChannel = testBlogChannelService.createAndSave();
+        testBlogChannelService.joinAsOwner(blogChannel, member);
+        post = testPostService.createAndSave(blogChannel, member);
+
+    }
+
+    @DisplayName("postId에 연관되어 있는 comment가 CreateAt Desc Order로 호출된다.")
+    @Test
+    void getCommentsForPostInDescOrder() {
+        Comment comment_1 = testCommentService.createAndSave(member, post);
+        Comment comment_2 = testCommentService.createAndSave(member, post);
+        CommentsWithAuthorResponse response = commentAssembler.getCommentResponseByPostId(post.getId());
+
+        Assertions.assertThat(response.authors().size()).isEqualTo(1);
+        Assertions.assertThat(response.comments().size()).isEqualTo(2);
+        //createdAt DESC order
+        Assertions.assertThat(response.comments().get(0).commentId()).isEqualTo(comment_2.getId());
+        Assertions.assertThat(response.comments().get(1).commentId()).isEqualTo(comment_1.getId());
+    }
 }
---
+++ src/test/java/com/zonbeozon/comment/service/CommentCreateTest.java
@@ -0,0 +1,43 @@
+package com.zonbeozon.comment.service;
+
+import com.zonbeozon.base.AbstractChannelIntegrationTest;
+import com.zonbeozon.channel.entity.BlogChannel;
+import com.zonbeozon.comment.entity.Comment;
+import com.zonbeozon.comment.repository.CommentRepository;
+import com.zonbeozon.member.domain.Member;
+import com.zonbeozon.post.entity.Post;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CommentCreateTest extends AbstractChannelIntegrationTest {
+    @Autowired
+    private CommentCreator commentCreator;
+    @Autowired
+    private CommentRepository commentRepository;
+
+    private Member member;
+    private BlogChannel blogChannel;
+    private Post post;
+
+    @BeforeEach
+    void setup() {
+        member = testMemberService.createAndSave();
+        blogChannel = testBlogChannelService.createAndSave();
+        testBlogChannelService.joinAsOwner(blogChannel, member);
+        post = testPostService.createAndSave(blogChannel, member);
+
+    }
+
+    @Test
+    @DisplayName("comment 정상 생성")
+    void CreateCommentSuccessfully() {
+        Long commentId = commentCreator.addComment(member.getId(), post.getId(), "DUUMY");
+        Comment found = commentRepository.findById(commentId).orElseThrow();
+
+        Assertions.assertThat(found.getId()).isEqualTo(commentId);
+        Assertions.assertThat(found.getContent()).isEqualTo("DUUMY");
+    }
+}
