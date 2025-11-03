fc9b84fe57c30612b75678b7e8286e4211c49dea
jintakkim <mine407603@gmail.com>
2025-11-03 16:07:21 +0900

    feat: 집계를 위한 Reaction 필드 추가

--- src/main/java/com/zonbeozon/reaction/post/entity/PostReaction.java
+++ src/main/java/com/zonbeozon/reaction/post/entity/PostReaction.java
@@ -13,6 +13,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "id", callSuper = false)
 @Getter
+@Table(
+        name = "post_reaction",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_post_author_type",
+                        // 사용자는 포스트 1개에 대해 1개의 리액션만 가능
+                        columnNames = {"post_id", "author_id"}
+                )
+        },
+        indexes = {
+                // 집계를 위한 인덱스
+                @Index(name = "idx_aggregated_post_type",
+                        columnList = "aggregated, post_id, reactionType")
+        }
+)
 public class PostReaction extends Reaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +37,14 @@ public class PostReaction extends Reaction {
     @JoinColumn(name = "post_id")
     @NotNull
     private Post post;
+
+    /**
+     * 이 Reaction이 PostMetric에 집계되었는지 여부.
+     * false: 아직 집계되지 않음 (실시간 집계 대상)
+     * true:  집계 완료
+     */
+    @Column(nullable = false)
+    private boolean aggregated = false;
 
     private PostReaction(
             Post post,
