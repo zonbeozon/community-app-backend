06a4ddac89f350f58046f9a97157790cde702429
jintakkim <mine407603@gmail.com>
2025-11-12 13:13:49 +0900

    change: 집계를 위한 필드 제거
    구현에 가까운 집계를 위한 필드를 제거한다.

--- src/main/java/com/zonbeozon/post/domain/metric/PostMetric.java
+++ src/main/java/com/zonbeozon/post/domain/metric/PostMetric.java
@@ -1,5 +1,6 @@
-package com.zonbeozon.post.domain;
+package com.zonbeozon.post.domain.metric;
 
+import com.zonbeozon.post.domain.Post;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -15,8 +16,8 @@ public class PostMetric {
     @JoinColumn(name = "post_id")
     private Post post;
     private Long viewCount = 0L;
-//    private Long likeCount = 0L;
-//    private Long viewCount = 0L;
+    private Long likeCount = 0L;
+    private Long dislikeCount = 0L;
 //    private Long commentCount = 0L;
 //    private Double contentScore = 0.0;
 //    private Double totalScore = 0.0;
--- src/main/java/com/zonbeozon/reaction/post/entity/PostReaction.java
+++ src/main/java/com/zonbeozon/reaction/post/entity/PostReaction.java
@@ -21,11 +21,6 @@ import lombok.NoArgsConstructor;
                         // 사용자는 포스트 1개에 대해 1개의 리액션만 가능
                         columnNames = {"post_id", "author_id"}
                 )
-        },
-        indexes = {
-                // 집계를 위한 인덱스
-                @Index(name = "idx_aggregated_post_type",
-                        columnList = "aggregated, post_id, reactionType")
         }
 )
 public class PostReaction extends Reaction {
@@ -38,17 +33,9 @@ public class PostReaction extends Reaction {
     @NotNull
     private Post post;
 
-    /**
-     * 이 Reaction이 PostMetric에 집계되었는지 여부.
-     * false: 아직 집계되지 않음 (실시간 집계 대상)
-     * true:  집계 완료
-     */
-    @Column(nullable = false)
-    private boolean aggregated = false;
-
     private PostReaction(
             Post post,
-            ReactionType reactionType,
+            ReactionType  reactionType,
             Member author
     ) {
         super(reactionType, author);
